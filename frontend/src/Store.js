db6da83f15603c6773be05302b355b7a77e8d978
SerhiiLemkin <Jackflit51997@gmail.com>
2023-04-18 13:47:50 +0300

    fixed bug with json parsing and pictures fixed

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -3,26 +3,25 @@ import { useReducer, createContext, storeReducer } from './Imports';
 export const Store = createContext();
 
 const initialState = {
-    cart: {
-        cartItems: localStorage.getItem('cartItems') ?
-            JSON.parse(localStorage.getItem('cartItems'))
-            :
-            [],
-        shippingAddress: localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')) 
-        : 
-        {},
-        paymentMethod: localStorage.getItem('paymentMethod') ? JSON.parse(localStorage.getItem('paymentMethod'))
-        :
-        ''      
-    },
-    userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+  cart: {
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
+    shippingAddress: localStorage.getItem('shippingAddress')
+      ? JSON.parse(localStorage.getItem('shippingAddress'))
+      : {},
+    paymentMethod: localStorage.getItem('paymentMethod')
+      ? localStorage.getItem('paymentMethod')
+      : '',
+  },
+  userInfo: localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : null,
 };
 
 export function StoreProvider(props) {
-    const [state, dispatch] = useReducer(storeReducer, initialState);
-    const body = { state, dispatch };
+  const [state, dispatch] = useReducer(storeReducer, initialState);
+  const body = { state, dispatch };
 
-    return <Store.Provider value={body}>
-        {props.children}
-    </Store.Provider>
+  return <Store.Provider value={body}>{props.children}</Store.Provider>;
 }
