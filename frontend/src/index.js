390199c2b60e6a8e0f4a0e6c2fda5a4649f3b6d1
SerhiiLemkin <Jackflit51997@gmail.com>
2023-06-25 12:37:22 +0300

    fixed url

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import {axios, React, ReactDOM, App, reportWebVitals, HelmetProvider, StoreProvider} from './Imports';
+import {
+  axios,
+  React,
+  ReactDOM,
+  App,
+  reportWebVitals,
+  HelmetProvider,
+  StoreProvider,
+} from './Imports';
 
-axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL = 'https://shopbackend-q435.onrender.com';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
